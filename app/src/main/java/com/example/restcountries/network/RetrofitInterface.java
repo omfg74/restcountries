de68a7fb4ae26642efe47971f753407336c9ece4
omfg <podsh74@gmail.com>
2019-04-26 18:00:47 +0500

    123

--- app/src/main/java/com/example/restcountries/network/RetrofitInterface.java
+++ app/src/main/java/com/example/restcountries/network/RetrofitInterface.java
@@ -4,8 +4,11 @@ import android.graphics.Bitmap;
 
 import com.example.restcountries.model.county.Country;
 import io.reactivex.Completable;
+import io.reactivex.Observable;
 import io.reactivex.Single;
 import java.util.List;
+
+
 import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.reactivex.disposables.Disposable;
@@ -18,8 +21,8 @@ import retrofit2.http.Url;
 
 public interface RetrofitInterface {
     @GET("rest/v2/all")
-    Single<List<Country>> getAllCountries();
+    Observable<List<Country>> getAllCountries();
 
     @GET//THis was the simliest way to use. It can be done also using regex or smth else
-    Single<Bitmap> getFlag(@Url() String flag);
+    Observable<Bitmap> getFlag(@Url() String flag);
 }
--- app/src/main/java/com/example/restcountries/presenter/MainActivityPresenter.java
+++ app/src/main/java/com/example/restcountries/presenter/MainActivityPresenter.java
@@ -17,6 +17,7 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
@@ -28,6 +29,10 @@ public class MainActivityPresenter implements MainActivityContract.Presenter {
     MainActivityContract.View view;
     MainActivityContract.Model realmCounrty;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
+//    private ConnectableObservable <List<Bitmap>> flagsObservable = getFlags(new String()) ;
+
+
+
     private RetrofitInterface retrofitInterface;
 
     public MainActivityPresenter(MainActivityContract.View view) {
@@ -45,77 +50,90 @@ public class MainActivityPresenter implements MainActivityContract.Presenter {
     }
 
     private void loadAllData(){
-//        List<Country>countries
+try {
+    compositeDisposable.add(retrofitInterface
+            .getAllCountries()
+            .subscribeOn(Schedulers.io())
+//            .flattenAsObservable((Function<List<Country>, Iterable<?>>) countries -> countries)
+            .flatMap(new Function<List<Country>, ObservableSource<?>>() {
+                @Override
+                public ObservableSource<?> apply(List<Country> countries) throws Exception {
+                    return null;
+                }
+            })
+            .subscribe());
+}catch (Exception e){e.printStackTrace();}
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribeWith(new DisposableSingleObserver<List<Country>>() {
+//                    @Override
+//                    public void onSuccess(List<Country> countries) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//
+//                    }
+//                }));
 
-        compositeDisposable.add(retrofitInterface
-                .getAllCountries()
-                .subscribeOn(Schedulers.io())
-                .flattenAsObservable((Function<List<Country>, Iterable<?>>) countries -> countries)
-                .flatMap()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new DisposableSingleObserver<Country>() {
 
-                    @Override
-                    public void onSuccess(Country country) {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-                });
     }
-    private ObservableSource<?> loadFlags(String flag, String countryName){
+
+
+
+//    private Observable<Bitmap> getFlags(Country country) {
+//        return retrofitInterface.getFlag(country.getFlag())
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe();
+//        return  retrofitInterface.getFlag(flag)
+//                .toObservable()
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread());
+
+//    }
+    private void loadFlags(Country country){
 
 //        //load flag for each counrty and save to gallery
 
-        String filename = generateImageName(countryName);
+        String filename = generateImageName(country.getName());
 
         compositeDisposable.add(retrofitInterface
-                .getFlag(flag)
+                .getFlag(country.getFlag())
                 .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new DisposableSingleObserver<Bitmap>() {
-                    @Override
-                    public void onSuccess(Bitmap bitmap) {
-                        Logger.toLog("bitmap "+bitmap.toString());
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                        Logger.toLog("failed load itmap");
-                        Logger.toLog("on failure");
-                        Logger.toLog(""+e.getMessage());
-                        Logger.toLog(""+e.getCause());
-                        Logger.toLog(""+e.getStackTrace());
-                    }
-                }));
-
+                .observeOn(AndroidSchedulers.mainThread()).subscribe());
     }
+
 
     private void writeToRealm(final List<Country>countries){
         //write data from internet to realm
         Realm realm = Realm.getDefaultInstance();
-        realm.beginTransaction();
-        realm.executeTransactionAsync((realm1) -> {
-            for (int i = 0; i < countries.size(); i++) {
-                realmCounrty = realm1.createObject(RealmCounty.class);
-                RealmList<RealmCurrency> realmCurrencyList = new RealmList<>();
-                realmCounrty.setName(countries.get(i).getName());
-                realmCounrty.setCapital(countries.get(i).getCapital());
-                for (int j = 0; j < countries.get(i).getCurrencies().size(); j++) {
-                    RealmCurrency realmCurrency = realm.createObject(RealmCurrency.class);
-                    realmCurrency.setName(countries.get(i).getCurrencies().get(j).getName());
-                    realmCurrency.setCode(countries.get(i).getCurrencies().get(j).getCode());
-                    realmCurrency.setSymbol(countries.get(i).getCurrencies().get(j).getSymbol());
-                    realmCurrencyList.add(realmCurrency);
+//        realm.beginTransaction();
+        try {
+            realm.executeTransactionAsync((realm1) -> {
+                for (int i = 0; i < countries.size(); i++) {
+                    realmCounrty = realm1.createObject(RealmCounty.class);
+                    RealmList<RealmCurrency> realmCurrencyList = new RealmList<>();
+                    realmCounrty.setName(countries.get(i).getName());
+                    realmCounrty.setCapital(countries.get(i).getCapital());
+                    for (int j = 0; j < countries.get(i).getCurrencies().size(); j++) {
+                        RealmCurrency realmCurrency = realm.createObject(RealmCurrency.class);
+                        realmCurrency.setName(countries.get(i).getCurrencies().get(j).getName());
+                        realmCurrency.setCode(countries.get(i).getCurrencies().get(j).getCode());
+                        realmCurrency.setSymbol(countries.get(i).getCurrencies().get(j).getSymbol());
+                        realmCurrencyList.add(realmCurrency);
+                    }
+                    realmCounrty.setCurrency(realmCurrencyList);
+                    realmCounrty.setFlagLink(countries.get(i).getFlag());
                 }
-                realmCounrty.setCurrency(realmCurrencyList);
-                realmCounrty.setFlagLink(countries.get(i).getFlag());
-            }
-        });
+            });
+        }catch (Exception e){
+         e.printStackTrace();
+        }finally {
+
+
+            realm.close();
+        }
     }
 
     private void checkIfRealmIsEmpty(){
